c4ab4d7f8283258ba27b4347b3bd0bfcdfa06b96
Aloisio Jacobs <aloisiojacobs@pop-os.localdomain>
2021-02-23 01:46:03 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+
+app.get('/', (request, response) => {
+  return response.json({message :'Hello World'})
+})
+
+app.post('/', (request, response) => {
+  return response.json({message: 'Dados salvos com sucesso.'})
+})
+
+app.listen(3000, () => console.log('Server is running'))
